929e3e7ab46803dbf421be061ee33d0eb23da6b8
tiny1990 <gomax.pan@gmail.com>
2024-11-18 23:07:31 +0800

    Update catch.js

--- catch.js
+++ catch.js
@@ -8,7 +8,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/.*\.api\.moji\.com\/(sns\/json\/profile\/get_info_.+|json\/member_new\/homepage_info.+|user\/personal\/json\/profile_.+|flycard\/novice|shortvideo\/.+) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/mojitianqi.js
+^https?:\/\/.*\.api\.moji\.com\/(sns\/json\/profile\/get_info_.+|json\/member_new\/homepage_info.+|user\/personal\/json\/profile_.+|flycard\/novice|shortvideo\/.+) url script-response-body https://raw.githubusercontent.com/tiny1990/tiny1990.github.io/refs/heads/master/catch.js
 
 [mitm]
 hostname = *.api.moji.com
