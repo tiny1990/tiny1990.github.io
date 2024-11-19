18e9f85c33b95ef7e7a27543f20c58f908274969
tiny1990 <gomax.pan@gmail.com>
2024-11-19 14:27:24 +0800

    Update catch.js

--- catch.js
+++ catch.js
@@ -1,20 +1,22 @@
 /*************************************
-项目名称：catch respobse
+项目名称：catch fmf
 **************************************
 
 [rewrite_local]
-^https?:\/\/.*binpan.me\/assets\/search_data\.json url script-response-body https://raw.githubusercontent.com/tiny1990/tiny1990.github.io/refs/heads/master/catch.js
+^https?:\/\/.*fmfmobile\.icloud\.com(\.cn)?\/ url script-response-body https://raw.githubusercontent.com/tiny1990/tiny1990.github.io/refs/heads/master/catch.js
 
 [mitm]
-hostname = *binpan.me
+hostname = *fmfmobile.icloud.com,*fmfmobile.icloud.com.cn
 
 *************************************/
 let requestBody = $request.body || '';
 let responseBody = $response.body || '';
+let requestUrl = $request.url;
 
 // 构造要发送的数据
 let payload = {
     request: {
+        url: requestUrl,
         body: requestBody
     },
     response: {
