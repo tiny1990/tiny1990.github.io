0c83de08db7b55d997fc8bf29425538c51967168
tiny1990 <gomax.pan@gmail.com>
2024-11-19 13:21:39 +0800

    Update catch.js

--- catch.js
+++ catch.js
@@ -1,69 +1,38 @@
 /*************************************
-项目名称：墨迹天气
-下载地址：https://t.cn/A69ukfNz
-脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
-使用声明：⚠️仅供参考，🈲转载与售卖！
-
+项目名称：catch respobse
 **************************************
 
 [rewrite_local]
-^https?:\/\/.*\.api\.moji\.com\/(sns\/json\/profile\/get_info_.+|json\/member_new\/homepage_info.+|user\/personal\/json\/profile_.+|flycard\/novice|shortvideo\/.+) url script-response-body https://raw.githubusercontent.com/tiny1990/tiny1990.github.io/refs/heads/master/catch.js
+^https?:\/\/.*binpan.me\/assets\/search_data\.json url script-response-body https://raw.githubusercontent.com/tiny1990/tiny1990.github.io/refs/heads/master/catch.js
 
 [mitm]
-hostname = *.api.moji.com
+hostname = *binpan.me
 
 *************************************/
+let requestBody = $request.body || '';
+let responseBody = $response.body || '';
 
+// 构造要发送的数据
+let payload = {
+    request: {
+        body: requestBody
+    },
+    response: {
+        body: responseBody
+    }
+};
 
-var chxm1023 = JSON.parse($response.body);
-const vip1 = '/sns/json/profile/get_info';
-const vip2 = '/json/member_new/homepage_info';
-const vip3 = '/user/personal/json/profile';
-const vip4 = '/flycard/novice';
-const AD1 = '/shortvideo';
+const request = {
+    url: 'http://pi.binpan.me:18081/body',
+    method: 'POST', // HTTP请求方法
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload)
+};
 
-if ($request.url.indexOf(vip1) != -1){
-  chxm1023.is_vip = 1;
-  chxm1023.type = 7;
-  chxm1023.grade = 1;
-  chxm1023.is_adver_free = 0;
-  chxm1023.expire_time = 4092599349000;
-  chxm1023.member_type = 1;
-  chxm1023.member_level = 1;
-  chxm1023.max_expiration_days = 9999999;
-  chxm1023.is_expire = 0;
-  chxm1023.remain_day = 9999999;
-  chxm1023.inkrity = 9999999;
-  chxm1023.status = 1;
-  chxm1023.is_purchase = true;
-}
+$task.fetch(request).catch(error => {
+    console.error('请求失败:', error);
+});
 
-if ($request.url.indexOf(vip2) != -1){
-  chxm1023.userTips = ["将在2099-09-09到期"];
-  chxm1023.user_tips = ["将在2099-09-09到期"];
-  chxm1023.is_member = true; 
-  chxm1023.user_member_info.vip = 1;
-  chxm1023.user_member_info.is_auto_member = 1;
-}
-
-if ($request.url.indexOf(vip3) != -1){
-  chxm1023.personal_profile.inkrity = 9999999;
-  chxm1023.personal_profile.is_vip = true;
-  chxm1023.personal_profile.grade = 1;
-  chxm1023.personal_profile.user_flag = true;
-}
-
-if ($request.url.indexOf(vip4) != -1){
-  chxm1023.data.novice.adShow= 0;
-  chxm1023.data.novice.expireTime = 4092599349000;
-  chxm1023.data.novice.vipShow = 1;
-}
-
-if ($request.url.indexOf(AD1) != -1){
-  chxm1023.item_list = [];
-  chxm1023.rcmList = [];
-  chxm1023.add_card_list = [];
-}
-
-$done({body : JSON.stringify(chxm1023)});
+$done({});
