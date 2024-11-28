b27efc9d41defc75eedb46ac759b5ce6e88f6af0
tiny1990 <gomax.pan@gmail.com>
2024-11-28 20:19:02 +0800

    Create reject.js

---
+++ reject.js
@@ -0,0 +1,7 @@
+/*************************************
+项目名称：reject
+**************************************
+
+[rewrite_local]
+^https?:\/\/sgshort\.wechat\.com url reject-500
+^https?:\/\/sgminorshort\.wechat\.com url reject-500
