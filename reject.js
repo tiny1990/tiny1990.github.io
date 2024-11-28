f9416afb09674cc265960aeb308ec2b92de9616e
tiny1990 <gomax.pan@gmail.com>
2024-11-28 20:23:49 +0800

    Update reject.js

--- reject.js
+++ reject.js
@@ -1,5 +1,5 @@
 hostname = sgshort.wechat.com,sgminorshort.wechat.com
 
-^https?:\/\/sgshort\.wechat\.com url reject-500
-^https?:\/\/sgminorshort\.wechat\.com url reject-500
+^https?:\/\/sgshort\.wechat\.com url reject-200
+^https?:\/\/sgminorshort\.wechat\.com url reject-200
 
