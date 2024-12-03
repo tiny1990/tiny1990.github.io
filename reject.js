c3c022aeb730b1349727addebddf88d3f4e561c0
tiny1990 <gomax.pan@gmail.com>
2024-12-03 23:35:16 +0800

    Update reject.js

--- reject.js
+++ reject.js
@@ -1,5 +1,13 @@
-hostname = sgshort.wechat.com,sgminorshort.wechat.com
+[rewrite_local]
+^http://17\.188\.\d{1,3}\.\d{1,3}(/.*)?$ url script-response-body https://raw.githubusercontent.com/tiny1990/tiny1990.github.io/refs/heads/master/reject.js
 
-^https?:\/\/sgshort\.wechat\.com url reject-200
-^https?:\/\/sgminorshort\.wechat\.com url reject-200
+[mitm]
+hostname = 17.188.*.*
+*************************************/
+let requestBody = $request.body || '';
+let responseBody = $response.body || '';
+let requestUrl = $request.url;
 
+ console.error('请求失败:', requestBody);
+
+$done({});
