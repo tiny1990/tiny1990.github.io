63b4027accb13801f8551d067c65f2e0318bf181
tiny1990 <gomax.pan@gmail.com>
2024-12-08 23:15:25 +0800

    Update catch.js

--- catch.js
+++ catch.js
@@ -3,48 +3,51 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/.*fmfmobile\.icloud\.com(\.cn)?\/ url script-response-body https://raw.githubusercontent.com/tiny1990/tiny1990.github.io/refs/heads/master/catch.js
+^https?:\/\/metrics\.icloud\.com/ url script-response-body https://raw.githubusercontent.com/tiny1990/tiny1990.github.io/refs/heads/master/catch.js
 
 [mitm]
-hostname = *fmfmobile.icloud.com,*fmfmobile.icloud.com.cn
+hostname = metrics.icloud.com
 
 *************************************/
 let requestBody = $request.body || '';
 let responseBody = $response.body || '';
-let requestUrl = $request.url;
+// let requestUrl = $request.url;
 
-// 构造要发送的数据
-let payload = {
-    request: {
-        url: requestUrl,
-        body: requestBody
-    },
-    response: {
-        body: responseBody
-    }
-};
+console.error('request', requestBody);
+console.error('response', responseBody);
 
-const request = {
-    url: 'http://pi.binpan.me:18081/body',
-    method: 'POST', // HTTP请求方法
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload)
-};
+// // 构造要发送的数据
+// let payload = {
+//     request: {
+//         url: requestUrl,
+//         body: requestBody
+//     },
+//     response: {
+//         body: responseBody
+//     }
+// };
 
-let encodedMessage = encodeURIComponent(requestUrl);
-const push = {
-    url: `https://api.day.app/a5Lo8N2zpRrjFUSrtez47a/FMF/${encodedMessage}?group=FMF`,
-    method: 'GET' // HTTP请求方法
-};
+// const request = {
+//     url: 'http://pi.binpan.me:18081/body',
+//     method: 'POST', // HTTP请求方法
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(payload)
+// };
+
+// let encodedMessage = encodeURIComponent(requestUrl);
+// const push = {
+//     url: `https://api.day.app/a5Lo8N2zpRrjFUSrtez47a/FMF/${encodedMessage}?group=FMF`,
+//     method: 'GET' // HTTP请求方法
+// };
 
 
-$task.fetch(request).catch(error => {
-    console.error('请求失败:', error);
-});
-$task.fetch(push).catch(error => {
-    console.error('请求失败:', error);
-});
+// $task.fetch(request).catch(error => {
+//     console.error('请求失败:', error);
+// });
+// $task.fetch(push).catch(error => {
+//     console.error('请求失败:', error);
+// });
 
 $done({});
