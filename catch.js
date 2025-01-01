3c3bac4a2cc69268674c4bb7a958bf8e3cbe7b57
tiny1990 <gomax.pan@gmail.com>
2025-01-01 22:23:18 +0800

    Update catch.js

--- catch.js
+++ catch.js
@@ -11,21 +11,18 @@ hostname = metrics.icloud.com
 *************************************/
 let requestBody = $request.body || '';
 let responseBody = $response.body || '';
-// let requestUrl = $request.url;
+let requestUrl = $request.url;
 
-console.error('request', requestBody);
-console.error('response', responseBody);
-
-// // 构造要发送的数据
-// let payload = {
-//     request: {
-//         url: requestUrl,
-//         body: requestBody
-//     },
-//     response: {
-//         body: responseBody
-//     }
-// };
+// 构造要发送的数据
+let payload = {
+    request: {
+        url: requestUrl,
+        body: requestBody
+    },
+    response: {
+        body: responseBody
+    }
+};
 
 // const request = {
 //     url: 'http://pi.binpan.me:18081/body',
@@ -36,18 +33,18 @@ console.error('response', responseBody);
 //     body: JSON.stringify(payload)
 // };
 
-// let encodedMessage = encodeURIComponent(requestUrl);
-// const push = {
-//     url: `https://api.day.app/a5Lo8N2zpRrjFUSrtez47a/FMF/${encodedMessage}?group=FMF`,
-//     method: 'GET' // HTTP请求方法
-// };
+let encodedMessage = encodeURIComponent(requestUrl);
+const push = {
+    url: `https://api.day.app/a5Lo8N2zpRrjFUSrtez47a/FMF/${encodedMessage}?group=FMF`,
+    method: 'GET' // HTTP请求方法
+};
 
 
 // $task.fetch(request).catch(error => {
 //     console.error('请求失败:', error);
 // });
-// $task.fetch(push).catch(error => {
-//     console.error('请求失败:', error);
-// });
+$task.fetch(push).catch(error => {
+    console.error('请求失败:', error);
+});
 
 $done({});
