c88191663421fd96dce1f7ea498a7deba03623f8
tiny1990 <gomax.pan@gmail.com>
2025-08-31 20:02:04 +0800

    Update catch_cookie.js

--- catch_cookie.js
+++ catch_cookie.js
@@ -1,21 +1,9 @@
 
 /*************************************
 项目名称：catch fmf
-**************************************
+**************************************/
+let requestHeader = $request.headers || '';
 
-[rewrite_local]
-^https?:\/\/sealsuite\.bytedance\.com url script-response-body https://raw.githubusercontent.com/tiny1990/tiny1990.github.io/refs/heads/master/catch_cookie.js
-[mitm]
-hostname = sealsuite.bytedance.com
-
-*************************************/
-let requestBody = $request.body || '';
-let responseBody = $response.body || '';
-let requestHeaders = $request.headers;
-
-
-console.log('请求头信息:', JSON.stringify(request, null, 2));
-console.log('Cookie:', requestHeaders['Cookie']);
-
+$prefs.setValueForKey(requestHeader['Cookie'], "feiCookie");
 
 $done({});
