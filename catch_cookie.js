026b54e09cd6e947b2955c2b9f2250addac68283
tiny1990 <gomax.pan@gmail.com>
2025-08-31 17:39:31 +0800

    Update catch_cookie.js

--- catch_cookie.js
+++ catch_cookie.js
@@ -4,48 +4,17 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/.*fmfmobile\.icloud\.com(\.cn)?\/ url script-response-body https://raw.githubusercontent.com/tiny1990/tiny1990.github.io/refs/heads/master/catch_cookie.js
+^https://sealsuite\.bytedance\.com/api/v1/wifi/guest/apply/ url script-response-body https://raw.githubusercontent.com/tiny1990/tiny1990.github.io/refs/heads/master/catch_cookie.js
 [mitm]
-hostname = *fmfmobile.icloud.com,*fmfmobile.icloud.com.cn
+hostname = sealsuite.bytedance.com
 
 *************************************/
 let requestBody = $request.body || '';
 let responseBody = $response.body || '';
-let requestUrl = $request.url;
-
-// 构造要发送的数据
-let payload = {
-    request: {
-        url: requestUrl,
-        body: requestBody
-    },
-    response: {
-        body: responseBody
-    }
-};
-
-// const request = {
-//     url: 'http://pi.binpan.me:18081/body',
-//     method: 'POST', // HTTP请求方法
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(payload)
-// };
-
-console.error('请求失败:', payload);
-// let encodedMessage = encodeURIComponent(requestUrl);
-// const push = {
-//     url: `https://api.day.app/a5Lo8N2zpRrjFUSrtez47a/FMF/${encodedMessage}?group=FMF`,
-//     method: 'GET' // HTTP请求方法
-// };
+let requestHeaders = $request.headers;
 
 
-// // $task.fetch(request).catch(error => {
-// //     console.error('请求失败:', error);
-// // });
-// $task.fetch(push).catch(error => {
+console.error('请求失败:', requestHeaders);
 
-// });
 
 $done({});
