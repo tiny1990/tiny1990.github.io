cd81e29baa8bc214a2a64625f91f3f11a5a9f7fb
tiny1990 <gomax.pan@gmail.com>
2025-08-31 16:44:49 +0800

    Update catch_cookie.js

--- catch_cookie.js
+++ catch_cookie.js
@@ -3,7 +3,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/sealsuite.bytedance.com\/api\/v1\/wifi/guest\/apply url script-response-body https://raw.githubusercontent.com/tiny1990/tiny1990.github.io/refs/heads/master/catch_cookie.js
+^https?:\/\/sealsuite.bytedance.com\/api\/v1\/wifi/guest\/apply/ url script-response-body https://raw.githubusercontent.com/tiny1990/tiny1990.github.io/refs/heads/master/catch_cookie.js
 [mitm]
 hostname = sealsuite.bytedance.com
 
